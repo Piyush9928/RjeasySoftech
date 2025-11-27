e262f6279670dc1d3b62cf79f3abe909d0d9cbbc
Piyush Singh <ps1521155@gmail.com>
2025-11-27 14:46:56 +0530

    Added root test route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/auth");
-
+require("dotenv").config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", authRoutes);
+// root test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
+// API routes here
+app.use("/api", require("./routes/auth"));
+
+// Start Server
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`API running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
